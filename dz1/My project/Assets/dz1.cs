df1690109598cb02c9101c5d8000a3b47154088f
Katharsis464 <daniil.kholod80@gmail.com>
2023-10-01 18:48:33 +0300

    Update dz1.cs

--- dz1/My project/Assets/dz1.cs
+++ dz1/My project/Assets/dz1.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class dz1 : MonoBehaviour
 {
-    public float Speed = 10f;
+     
     public GameObject sphere;
     public GameObject cube;
     // Start is called before the first frame update
@@ -16,27 +16,7 @@ public class dz1 : MonoBehaviour
     // Update is called once per frame
     void FixedUpdate()
     {
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    sphere.SetActive(!sphere.activeSelf);
-            //transform.Translate(Vector3.forward * Speed * Time.deltaTime);
-            //Debug.Log(sphere.gameObject.activeSelf);
-            //if (sphere.activeSelf == false)
-            //{
-            //    sphere.SetActive(true);
-            //}
-            //if (sphere.activeSelf == true)
-            //{
-            //    sphere.SetActive(false);
-            //}
-            //Debug.Log(sphere.gameObject.activeSelf);
-        //}
-        
-        //if (Input.GetKey(KeyCode.F) && this.gameObject.activeSelf == false)
-        //{
-        //    this.gameObject.SetActive(true);
-        //}
-        
+         
     }
    public void  actsphere()
     {
